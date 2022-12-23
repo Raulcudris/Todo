7e7d04beef3ec9b519a81763c52b087a741f4fdd
Raul Cudris <raulcudriz@hotmail.com>
2022-12-23 09:28:17 -0500

    Primer commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TodoCounter.css';
+
+const TodoCounter = ({ total , completed }) =>
+{
+  return (
+    <h2 className='TodoCounter'>Has Completado {completed} de {total} TODOs</h2>
+  )
+}
+
+export { TodoCounter };
